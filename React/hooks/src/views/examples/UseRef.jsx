a7981660517009a0690bd40162a433ebd4af1fb2
Matheus Pereira <brasil.m.95@gmail.com>
2022-01-19 09:47:44 -0300

    useMemo e useRef

--- React/hooks/src/views/examples/UseRef.jsx
+++ React/hooks/src/views/examples/UseRef.jsx
@@ -43,7 +43,7 @@ const UseRef = (props) => {
                     onChange={e => setValue2(e.target.value)} />
             </div>
 
-            <SectionTitle title="Exercicio #3"/>
+            <SectionTitle title="Exercicio #3"/> 
 
 
 
