9012cd71e88d1c97cd43814ec21f637e79572568
Matheus Pereira <brasil.m.95@gmail.com>
2022-02-25 17:06:55 -0300

    up 24/02/22

--- Alura/html-css/projeto1/src/revisao-html-css/Header.jsx
+++ Alura/html-css/projeto1/src/revisao-html-css/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import './header.css'
+import logo from '../components/img/logo.png'
 
 export default props =>{
     return(
-        <div>
-            
-        </div>
+        <header className="header">
+            <img src={logo} />
+            <h1 className="title"> Barbearia Alura</h1>
+        </header>
     )
 }
